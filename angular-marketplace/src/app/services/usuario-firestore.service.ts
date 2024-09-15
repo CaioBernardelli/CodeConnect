d7e7dd502055299a4b85d6f410ba8f825689a71a
CaioBernardelli <caiojp2013@hotmail.com>
2024-09-15 13:04:35 -0300

    concertando layout

--- angular-marketplace/src/app/services/usuario-firestore.service.ts
+++ angular-marketplace/src/app/services/usuario-firestore.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collectionData, collection, addDoc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Firestore, collectionData, collection, addDoc, where, query, getDocs } from '@angular/fire/firestore';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { Usuario } from '../model/usuario';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { Usuario } from '../model/usuario';
 })
 export class UsuarioFirestoreService {
   private collectionName: string = 'usuarios'; 
+ 
 
   constructor(private firestore: Firestore) {}
 
@@ -16,18 +17,44 @@ export class UsuarioFirestoreService {
     return collectionData(usuariosCollection, { idField: 'id' }) as Observable<Usuario[]>;
   }
 
-  inserir(usuario: Usuario): Promise<void> {
+  async inserir(usuario: Usuario): Promise<void> {
+    // Espera a validação do e-mail antes de continuar
+    await this.validarEmailsDiferentes(usuario.email);
     
-    const { id, ...usuarioSemId } = usuario;
-    const usuariosCollection = collection(this.firestore, this.collectionName); // Referência à coleção 'usuarios'
-    return addDoc(usuariosCollection, usuarioSemId) // Adiciona um novo documento com os dados do usuário
-      .then(() => {
-        console.log("Usuário adicionado com sucesso!");
-      })
-      .catch((error) => {
-        console.error("Erro ao adicionar usuário: ", error);
-      });
+    // Realiza as outras validações
+    this.validarMaiorIdade(usuario);
 
+    const { id, ...usuarioSemId } = usuario;
+    const usuariosCollection = collection(this.firestore, this.collectionName);
     
+    // Adiciona o documento apenas após a validação bem-sucedida
+    return addDoc(usuariosCollection, usuarioSemId).then(() => {
+      // Sucesso, sem ações adicionais
+    });
+  }
+   // Validações Inserir 
+   private validarMaiorIdade(usuario: Usuario) {
+    if (usuario.idade < 18) {
+      throw new Error('Usuário não pode ser menor de idade!');
+    }
+  }
+
+  private validarEmail(email: string) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      throw new Error('Endereço de email inválido!');
+    }
+  }
+  
+
+   // Método que verifica se já existe um usuário com o mesmo email
+   async validarEmailsDiferentes(email: string): Promise<void> {
+    const usuariosCollection = collection(this.firestore, this.collectionName);
+    const q = query(usuariosCollection, where('email', '==', email));
+    const querySnapshot = await getDocs(q);
+    
+    if (!querySnapshot.empty) {
+      throw new Error('Usuário já cadastrado com o mesmo endereço de email!');
+    }
   }
 }
